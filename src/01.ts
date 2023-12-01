42b74d7f1078e89885d3a80c3b4cf4bd5e02fdc2
Christopher Oldfield <humantree@icloud.com>
2023-12-01 21:23:54 +1100

    Day 01-2

--- src/01.ts
+++ src/01.ts
@@ -2,10 +2,43 @@ import loadInputFile from "./common/loadInputFile"
 
 const input = await loadInputFile(1)
 
+const numberWords = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+} as const
+
+function parseNumber(number: string) {
+  const int = parseInt(number)
+  if (!Number.isNaN(int)) return int
+
+  if (numberWords[number as keyof typeof numberWords]) {
+    return numberWords[number as keyof typeof numberWords]
+  }
+
+  throw new Error(`Unable to parse string ${number}`)
+}
+
+const numberRegex = new RegExp(`\\d|${Object.keys(numberWords).join("|")}`, "g")
+
 const lineNumbers = input.map((line) => {
-  const matches = line.match(/\d/g)
+  const matches = []
+  let results
+
+  while ((results = numberRegex.exec(line))) {
+    matches.push(results[0])
+    numberRegex.lastIndex = results.index + 1
+  }
+
   if (!matches) throw new Error("No numbers found.")
-  const number = `${matches[0]}${matches[matches.length - 1]}`
+  const numberMatches = matches.map(parseNumber)
+  const number = `${numberMatches[0]}${numberMatches[matches.length - 1]}`
   return parseInt(number)
 })
 
