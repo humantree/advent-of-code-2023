73faa20c03089cc7d4dbe51ae6cb9e0a5ec0d534
Christopher Oldfield <humantree@icloud.com>
2023-12-04 17:26:19 +1100

    Day 04-2

--- src/04.ts
+++ src/04.ts
@@ -2,22 +2,28 @@ import loadInputFile from "./common/loadInputFile"
 
 const input = await loadInputFile(4)
 
-const cards = input.map((line) => {
-  const [winningNumbers, scratchedNumbers] = line
-    .split(":")[1]
-    .split("|")
-    .map((numbers) =>
-      numbers
-        .trim()
-        .split(/\s+/)
-        .map((number) => parseInt(number))
-    )
+type Card = {
+  id: number
+  matches: number[]
+  scratchedNumbers: number[]
+  winningNumbers: number[]
+}
+
+const cards: Card[] = input.map((line) => {
+  const [text, numbers] = line.split(":")
+  const id = parseInt(text.split(/\s+/)[1])
+  const [winningNumbers, scratchedNumbers] = numbers.split("|").map((numbers) =>
+    numbers
+      .trim()
+      .split(/\s+/)
+      .map((number) => parseInt(number))
+  )
 
   const matches = scratchedNumbers.filter((number) =>
     winningNumbers.includes(number)
   )
 
-  return { matches, scratchedNumbers, winningNumbers }
+  return { id, matches, scratchedNumbers, winningNumbers }
 })
 
 const pointsTotal = cards.reduce((acc, card) => {
@@ -27,3 +33,24 @@ const pointsTotal = cards.reduce((acc, card) => {
 
 console.log("How many points are the cards worth in total?")
 console.log(pointsTotal)
+
+const cardQueue = [...cards]
+let cardsCounted = 0
+let card
+
+while ((card = cardQueue.shift())) {
+  cardsCounted++
+
+  for (let i = 1; i <= card.matches.length; i++) {
+    const prizeCardId = card.id + i
+    const prizeCard = cards.find((card) => card.id === prizeCardId)
+    if (!prizeCard) throw new Error(`No card found with the ID ${prizeCardId}`)
+    cardQueue.push(prizeCard)
+  }
+}
+
+console.log()
+console.log(
+  "Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?"
+)
+console.log(cardsCounted)
